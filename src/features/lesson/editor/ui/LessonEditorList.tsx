1dbb2c11971f8d190ed4509f3741ac0b718bd758
7xthanim2 <7xthanim2@192.168.0.100>
2025-06-12 04:34:38 +0600

    011

--- src/features/lesson/editor/ui/LessonEditorList.tsx
+++ src/features/lesson/editor/ui/LessonEditorList.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { Box, Button, Card, Flex, IconButton, Text } from '@radix-ui/themes';
+import { Box, Card, Flex, IconButton, Text } from '@radix-ui/themes';
 import { Pencil1Icon, TrashIcon } from '@radix-ui/react-icons';
 import { Lesson } from '@shared/lib/indexedDB/types';
 import styles from './LessonEditorList.module.scss';
+import { useRouter } from 'next/navigation';
 
 interface LessonEditorListProps {
   lessons: Lesson[];
@@ -12,6 +13,7 @@ interface LessonEditorListProps {
 }
 
 export const LessonEditorList = ({ lessons, onEdit, onDelete }: LessonEditorListProps) => {
+  const router = useRouter();
   if (lessons.length === 0) {
     return (
       <Card className={styles.empty}>
@@ -20,17 +22,38 @@ export const LessonEditorList = ({ lessons, onEdit, onDelete }: LessonEditorList
     );
   }
 
+  const handleLessonChange = (value: string) => {
+    router.push(`/lesson/${value}`);
+  };
+
   return (
     <Box className={styles.list}>
       {lessons.map((lesson) => (
-        <Card key={lesson.id} className={styles.item}>
+        <Card
+          key={lesson.id}
+          className={styles.item}
+          onClick={(e) => {
+            handleLessonChange(lesson.id);
+          }}
+        >
           <Flex justify="between" align="center">
             <Text>{lesson.title}</Text>
             <Flex gap="2">
-              <IconButton onClick={() => onEdit(lesson)}>
+              <IconButton
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onEdit(lesson);
+                }}
+              >
                 <Pencil1Icon />
               </IconButton>
-              <IconButton color="red" onClick={() => onDelete(lesson)}>
+              <IconButton
+                color="red"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onDelete(lesson);
+                }}
+              >
                 <TrashIcon />
               </IconButton>
             </Flex>
@@ -39,4 +62,4 @@ export const LessonEditorList = ({ lessons, onEdit, onDelete }: LessonEditorList
       ))}
     </Box>
   );
-}; 
+};
